2f981a1dabbb5883c3b6eca3263fc37f358b0394
Wenke Xia <wenkexia@narval1.narval.calcul.quebec>
2026-09-17 01:29:33 -0400

    scrunch polarisation before clfd if necessary to reduce memory usage

--- backend/tools/stack_utils.py
+++ backend/tools/stack_utils.py
@@ -1,4 +1,5 @@
 import os
+import gc
 import copy
 import tqdm
 import shutil
@@ -13,6 +14,217 @@ from ..utils.logger import logger
 from ..utils.data_quality import data_quality_utils
 from ..processing.dspsr_shutils import dspsr_shutils
 from ..processing.archive_shutils import archive_shutils
+
+def _normalize_data(data):
+    """
+    Normalize the data by dividing each subint, pol, and freq by its maximum value.
+    Parameters:
+        data (np.ndarray): The data to normalize.
+    Returns:
+        np.ndarray: The normalized data.
+    """
+
+    for i_sub in range(data.shape[0]):
+        for i_pol in range(data.shape[1]):
+            for i_freq in range(data.shape[2]):
+                this_mean = np.nanmean(data[i_sub, i_pol, i_freq])
+                this_std = np.nanstd(data[i_sub, i_pol, i_freq])
+                if this_std != 0:
+                    data[i_sub, i_pol, i_freq] -= (this_mean)
+                    data[i_sub, i_pol, i_freq] /= this_std
+                    # data[i_sub, i_pol, i_freq] /= this_mean
+                else:
+                    data[i_sub, i_pol, i_freq] = np.zeros_like(data[i_sub, i_pol, i_freq])
+
+    return data
+
+def _resize_array(input_array, new_shape):
+    """
+    Resize an N-dimensional numpy array to a new shape using interpolation.
+
+    Parameters:
+        input_array (np.ndarray): The array to resize.
+        new_shape (tuple): The desired shape (must be same number of dimensions as input).
+        order (int): Interpolation order (0=nearest, 1=linear, 3=cubic, etc.)
+
+    Returns:
+        np.ndarray: Resized array.
+    """
+    input_shape = np.array(input_array.shape)
+    new_shape = np.array(new_shape)
+
+    if len(input_shape) != len(new_shape):
+        raise ValueError("new_shape must have same number of dimensions as input_array")
+
+    if np.all(input_shape == new_shape):
+        return input_array
+
+    return zoom(input_array, new_shape / input_shape, order=1) # Use scipy's built-in resize function
+
+def _resize_psrchive(ar_obj, new_shape):
+    """
+    Resize a PSRCHIVE archive to a new shape using PSRCHIVE's built-in scrunch function.
+    Parameters:
+        ar_obj (psrchive.Archive): The PSRCHIVE archive object to resize.
+        new_shape (tuple): The desired shape (must be same number of dimensions as input).
+    Returns:
+        str: The path to the resized PSRCHIVE archive.
+    """
+
+    # Get shapes
+    data_n_subs, data_n_pols, data_n_freqs, data_n_bins = ar_obj.get_data().shape
+    new_n_subs, new_n_pols, new_n_freqs, new_n_bins = new_shape
+    # subints, pols, freqs, bins
+
+    # Scrunch pols
+    if new_n_pols <= 1:
+        ar_obj.pscrunch() # it's only physically make sense to scrunch into 1 pol 
+        
+    # Scrunch subints
+    if new_n_subs == 1:
+        ar_obj.tscrunch()
+    elif new_n_subs < data_n_subs:
+        ar_obj.tscrunch_to_nsub(new_n_subs)
+
+    # Scrunch freqs
+    if new_n_freqs == 1:
+        ar_obj.fscrunch()
+    elif new_n_freqs < data_n_freqs:
+        ar_obj.fscrunch_to_nchan(new_n_freqs)
+
+    # Scrunch bins
+    if new_n_bins < data_n_bins:
+        ar_obj.bscrunch_to_nbin(new_n_bins)
+    
+    return ar_obj
+
+def _stack_worker(file_info, normalize, config, logger):
+    """
+    Worker function to stack the data from the input archive.
+    Parameters:
+        file_info (dict): Information about the input file, including location and backend.
+        normalize (bool): Whether to normalize the data.
+        config (dict): Configuration dictionary containing various settings 
+            (jumps, tempdir, parfile, remove_baseline, n_subs, n_pols, n_freqs, n_bins).
+        logger (logging.Logger): Logger for logging messages.
+
+    Returns:
+        str: Path to the output stacked file.
+    """
+
+    this_outfile = ""
+
+    # Check if the file info is valid
+    if "location" not in file_info or "backend" not in file_info:
+        logger.error(f"File info {file_info} does not contain location or backend information. Skipping.")
+        return this_outfile
+
+    try:
+        # read file info and jump
+        this_location = file_info["location"]
+        this_backend = file_info["backend"]
+        this_jump = 0
+        if this_backend in config["jumps"]:
+            this_jump = config["jumps"][this_backend][0]
+        
+        # create workspace for this archive
+        this_workspace = os.path.join(config["tempdir"], os.path.basename(this_location))
+        this_outfile = os.path.join(config["tempdir"], "stackfile__" + os.path.basename(this_location) + ".npz")
+        this_ar = this_workspace + "/data"
+        this_par = this_workspace + "/parfile"
+        if not os.path.exists(this_workspace):
+            os.makedirs(this_workspace)
+            
+        # prepare the archive
+        shutil.copy(this_location, this_ar)
+        shutil.copy(config["parfile"], this_par)
+
+        # convert filterbank data as needed
+        if this_location.endswith(".fil"):
+            this_fil = this_workspace + "/data.fil"
+            
+            # copy data to workspace
+            shutil.move(this_ar, this_fil)
+
+            # convert to ar using dspsr
+            du = dspsr_shutils(n_pools=1)
+            du.fil2ar(this_fil, this_ar, this_par, nbin=config["n_bins"])
+
+            # rename converted ar
+            shutil.move(this_ar + ".ar", this_ar)
+
+            # remove the filterbank data
+            os.remove(this_fil)
+            del this_fil
+
+        # scrunch polarization before clfd if required to save memory
+        if config["n_pols"] == 1:
+            this_arch = psrchive.Archive_load(this_ar) # load the archive
+            this_arch.dedisperse() # dedisperse
+            this_arch.pscrunch() # scrunch polarization
+            this_arch.unload(this_ar) # overwrite the archive
+            del this_arch # remove archive object
+            gc.collect() # clean up memory
+        
+        # initialize archive shutils
+        ashu = archive_shutils(this_ar)
+
+        # zap rfi
+        ashu.clfd()
+
+        # install parfile
+        ashu.install_parfile(this_par, jump=this_jump)
+
+        # load the archive
+        this_arch = psrchive.Archive_load(this_ar)
+
+        # dedisperse
+        this_arch.dedisperse()
+
+        # remove baseline if needed
+        if config["remove_baseline"]:
+            this_arch.remove_baseline()
+
+        # downsample using PSRCHIVE scrunch
+        _resize_psrchive(this_arch, (config["n_subs"], config["n_pols"], config["n_freqs"], config["n_bins"]))
+
+        # get duration
+        this_duration = (this_arch.end_time() - this_arch.start_time()).in_days()
+
+        # get the data
+        this_data = this_arch.get_data()
+
+        # use float32 for memory efficiency
+        this_data = this_data.astype(np.float32)
+
+        # free the archive from memory
+        del this_arch
+        gc.collect()
+            
+        # upsample using scipy as needed
+        if this_data.shape != (config["n_subs"], config["n_pols"], config["n_freqs"], config["n_bins"]):
+            logger.warning(f"Upsampling {this_ar} from {this_data.shape} to {(config['n_subs'], config['n_pols'], config['n_freqs'], config['n_bins'])}", layer=1)
+            this_data = _resize_array(this_data, (config["n_subs"], config["n_pols"], config["n_freqs"], config["n_bins"]))
+
+        # normalize
+        if normalize:
+            this_data = _normalize_data(this_data)
+
+        # get_snr
+        this_profile = np.sum(this_data, axis=(0, 1, 2))
+        this_snr = data_quality_utils.boxcar_snr(this_profile)
+
+        # save data to tempdir
+        np.savez(this_outfile, data=this_data, duration=this_duration, snr=this_snr)
+        
+    except Exception:
+        logger.warning(f"Thread {this_location} failed. Please refer to the traceback below")
+        logger.warning(traceback.format_exc())
+    
+    # remove the workspace
+    shutil.rmtree(this_workspace, ignore_errors=True)
+
+    return this_outfile
 
 class stack_utils():
     def __init__(self, files, parfile, n_subs=16, n_pols=3, n_freqs=1024, n_bins=1024, n_pools=4, jumps={}, remove_baseline=False, workspace="/tmp", logger=logger()):
@@ -47,14 +259,27 @@ class stack_utils():
         # Create tempdir
         os.makedirs(self.tempdir)
 
+        # Create worker config
+        worker_config = {
+            "jumps": self.jumps,
+            "tempdir": self.tempdir,
+            "parfile": self.parfile,
+            "remove_baseline": self.remove_baseline,
+            "n_subs": self.n_subs,
+            "n_pols": self.n_pols,
+            "n_freqs": self.n_freqs,
+            "n_bins": self.n_bins,
+            "logger": self.logger
+        }
+
         # Run stacking
         try: # Tempdir can still be cleanned up when task failed. 
             with Pool(self.n_pools) as pool:
                 stack_files = pool.starmap(
-                    self._stack, 
-                    tqdm.tqdm([(file_info, normalize) for file_info in self.files], desc="Preparing archives")
+                    _stack_worker, 
+                    tqdm.tqdm([(file_info, normalize, worker_config, self.logger.copy()) for file_info in self.files], desc="Preparing archives")
                 )
-                
+
             for f in tqdm.tqdm(stack_files, desc="Stacking archives"):
                 if not os.path.exists(f):
                     self.logger.warning(f"Stack file {f} does not exist. The processing thread might be failed or OOM killed. ")
@@ -68,6 +293,10 @@ class stack_utils():
                 # Get the duration and snr
                 self.durations.append(this_data["duration"])
                 self.snrs.append(this_data["snr"])
+
+                # Remove this_data from memory
+                del this_data
+                gc.collect()
 
                 # Remove the stack file
                 os.remove(f)
@@ -83,197 +312,6 @@ class stack_utils():
         # Print the results
         self.logger.info(f"Stacked {self.n_stacked} out of {len(self.files)} files.")
         self.logger.info(f"Stacked data shape: {self.stacked_data.shape}")
-    
-    def _stack(self, file_info, normalize=True):
-        """
-        Stack the data from the input archive.
-        """
-
-        this_outfile = ""
-
-        # Check if the file info is valid
-        if "location" not in file_info or "backend" not in file_info:
-            self.logger.error(f"File info {file_info} does not contain location or backend information. Skipping.")
-            return this_outfile
-
-        try:
-            # read file info and jump
-            this_location = file_info["location"]
-            this_backend = file_info["backend"]
-            this_jump = 0
-            if this_backend in self.jumps:
-                this_jump = self.jumps[this_backend][0]
-            
-            # create workspace for this archive
-            this_workspace = os.path.join(self.tempdir, os.path.basename(this_location))
-            this_outfile = os.path.join(self.tempdir, "stackfile__" + os.path.basename(this_location) + ".npz")
-            this_ar = this_workspace + "/data"
-            this_par = this_workspace + "/parfile"
-            if not os.path.exists(this_workspace):
-                os.makedirs(this_workspace)
-                
-            # prepare the archive
-            shutil.copy(this_location, this_ar)
-            shutil.copy(self.parfile, this_par)
-
-            # convert filterbank data as needed
-            if this_location.endswith(".fil"):
-                this_fil = this_workspace + "/data.fil"
-                
-                # copy data to workspace
-                shutil.move(this_ar, this_fil)
-    
-                # convert to ar using dspsr
-                du = dspsr_shutils(n_pools=1)
-                du.fil2ar(this_fil, this_ar, this_par, nbin=self.n_bins)
-
-                # rename converted ar
-                shutil.move(this_ar + ".ar", this_ar)
-    
-                # remove the filterbank data
-                os.remove(this_fil)
-                del this_fil
-                
-    
-            # # downsample the archive first as needed to save memory
-            # this_arch = psrchive.Archive_load(this_ar) # load the archive
-            # this_arch.dedisperse() # dedisperse
-            # self.resize_psrchive(this_arch, (self.n_subs, self.n_pols, self.n_freqs, self.n_bins)) # downsample using PSRCHIVE scrunch
-            # this_arch.unload(this_ar) # overwrite the archive
-            
-            # initialize archive shutils
-            ashu = archive_shutils(this_ar)
-    
-            # zap rfi
-            ashu.clfd()
-    
-            # install parfile
-            ashu.install_parfile(this_par, jump=this_jump)
-    
-            # load the archive
-            this_arch = psrchive.Archive_load(this_ar)
-    
-            # dedisperse
-            this_arch.dedisperse()
-
-            # remove baseline if needed
-            if self.remove_baseline:
-                this_arch.remove_baseline()
-    
-            # downsample using PSRCHIVE scrunch
-            self.resize_psrchive(this_arch, (self.n_subs, self.n_pols, self.n_freqs, self.n_bins))
-    
-            # get the data
-            this_data = this_arch.get_data()
-                
-            # upsample using scipy as needed
-            if this_data.shape != (self.n_subs, self.n_pols, self.n_freqs, self.n_bins):
-                self.logger.warning(f"Upsampling {this_ar} from {this_data.shape} to {(self.n_subs, self.n_pols, self.n_freqs, self.n_bins)}", layer=1)
-                this_data = self.resize_array(this_data, (self.n_subs, self.n_pols, self.n_freqs, self.n_bins))
-    
-            # normalize
-            if normalize:
-                this_data = self.normalize_data(this_data)
-
-            # get_snr
-            this_profile = np.sum(this_data, axis=(0, 1, 2))
-            this_snr = data_quality_utils.boxcar_snr(this_profile)
-
-            # get duration
-            this_duration = (this_arch.end_time() - this_arch.start_time()).in_days()
-    
-            # save data to tempdir
-            np.savez(this_outfile, data=this_data, duration=this_duration, snr=this_snr)
-            
-        except Exception:
-            self.logger.warning(f"Thread {this_location} failed. Please refer to the traceback below")
-            self.logger.warning(traceback.format_exc())
-        
-        # remove the workspace
-        shutil.rmtree(this_workspace, ignore_errors=True)
-
-        return this_outfile
-
-    def normalize_data(self, data):
-        """
-        Normalize the data by dividing each subint, pol, and freq by its maximum value.
-        Parameters:
-            data (np.ndarray): The data to normalize.
-        Returns:
-            np.ndarray: The normalized data.
-        """
-
-        for i_sub in range(data.shape[0]):
-            for i_pol in range(data.shape[1]):
-                for i_freq in range(data.shape[2]):
-                    this_mean = np.nanmean(data[i_sub, i_pol, i_freq])
-                    this_std = np.nanstd(data[i_sub, i_pol, i_freq])
-                    if this_std != 0:
-                        data[i_sub, i_pol, i_freq] -= (this_mean)
-                        data[i_sub, i_pol, i_freq] /= this_std
-                        # data[i_sub, i_pol, i_freq] /= this_mean
-                    else:
-                        data[i_sub, i_pol, i_freq] = np.zeros_like(data[i_sub, i_pol, i_freq])
-
-        return data
-
-    def resize_psrchive(self, ar_obj, new_shape):
-        """
-        Resize a PSRCHIVE archive to a new shape using PSRCHIVE's built-in scrunch function.
-        Parameters:
-            ar_obj (psrchive.Archive): The PSRCHIVE archive object to resize.
-            new_shape (tuple): The desired shape (must be same number of dimensions as input).
-        Returns:
-            str: The path to the resized PSRCHIVE archive.
-        """
-
-        # Get shapes
-        data_n_subs, data_n_pols, data_n_freqs, data_n_bins = ar_obj.get_data().shape
-        new_n_subs, new_n_pols, new_n_freqs, new_n_bins = new_shape
-        # subints, pols, freqs, bins
-
-        # Scrunch pols
-        if new_n_pols <= 1:
-            ar_obj.pscrunch() # it's only physically make sense to scrunch into 1 pol 
-            
-        # Scrunch subints
-        if new_n_subs == 1:
-            ar_obj.tscrunch()
-        elif new_n_subs < data_n_subs:
-            ar_obj.tscrunch_to_nsub(new_n_subs)
-
-        # Scrunch freqs
-        if new_n_freqs == 1:
-            ar_obj.fscrunch()
-        elif new_n_freqs < data_n_freqs:
-            ar_obj.fscrunch_to_nchan(new_n_freqs)
-
-        # Scrunch bins
-        if new_n_bins < data_n_bins:
-            ar_obj.bscrunch_to_nbin(new_n_bins)
-
-    def resize_array(self, input_array, new_shape):
-        """
-        Resize an N-dimensional numpy array to a new shape using interpolation.
-
-        Parameters:
-            input_array (np.ndarray): The array to resize.
-            new_shape (tuple): The desired shape (must be same number of dimensions as input).
-            order (int): Interpolation order (0=nearest, 1=linear, 3=cubic, etc.)
-
-        Returns:
-            np.ndarray: Resized array.
-        """
-        input_shape = np.array(input_array.shape)
-        new_shape = np.array(new_shape)
-
-        if len(input_shape) != len(new_shape):
-            raise ValueError("new_shape must have same number of dimensions as input_array")
-
-        if np.all(input_shape == new_shape):
-            return input_array
-
-        return zoom(input_array, new_shape / input_shape, order=1) # Use scipy's built-in resize function
     
     def get_data(self, n_subs=None, n_pols=None, n_freqs=None, n_bins=None, normalize=False):
         data_shape = [self.n_subs, self.n_pols, self.n_freqs, self.n_bins]
@@ -301,13 +339,13 @@ class stack_utils():
 
         if data_shape == (self.n_subs, self.n_pols, self.n_freqs, self.n_bins): 
             if normalize:
-                return self.normalize_data(data_copy)
+                return _normalize_data(data_copy)
             return data_copy
 
         if normalize:
-            return self.normalize_data(self.resize_array(data_copy, data_shape))
+            return _normalize_data(_resize_array(data_copy, data_shape))
             
-        return self.resize_array(data_copy, data_shape)
+        return _resize_array(data_copy, data_shape)
 
     def get_stacked_snr(self):
         if self.n_bins <= 1:
